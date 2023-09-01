c4b261d63f20c2d245a6e7ca200d5a90726e6175
pasan1227 <pasanratnayake@gmail.com>
2023-09-01 22:28:58 +0530

    categories initialized

--- src/App.js
+++ src/App.js
@@ -1,5 +1,42 @@
 const App = () => {
-  return <div>Hello world</div>;
+  const categories = [
+    {
+      id: 1,
+      title: "Hats",
+    },
+    {
+      id: 2,
+      title: "Jackets",
+    },
+    {
+      id: 3,
+      title: "Sneakers",
+    },
+    {
+      id: 4,
+      title: "Womens",
+    },
+    {
+      id: 5,
+      title: "Mens",
+    },
+  ];
+
+  return (
+    <div className="categories-container">
+      {categories.map(({ title }) => {
+        return (
+          <div className="category-container">
+            <div className="background-image" />
+            <div className="category-body-container">
+              <h2>{title}</h2>
+              <p>Shop Now</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default App;
