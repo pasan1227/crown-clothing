3f61b0bbb3317c55eb9bbcab997eb7a184696e69
pasan1227 <pasanratnayake@gmail.com>
2023-09-02 13:48:03 +0530

    Navigation component & styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import { Routes, Route } from "react-router-dom";
+
 import Home from "./Routes/Home/home.component";
+import Navigation from "./Routes/Navigation/navigation.component";
+
+const Shop = () => {
+  return <h1>Shop</h1>;
+};
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
     </Routes>
   );
 };
