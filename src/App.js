1d70e66fb16854c55348e150904d94a41e05645e
pasan1227 <pasanratnayake@gmail.com>
2023-09-12 11:01:34 +0530

    Sign in form

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 
 import Home from "./Routes/Home/home.component";
 import Navigation from "./Routes/Navigation/navigation.component";
-import SignIn from "./Routes/Sign-in/sign-in.component";
+import Authentication from "./Routes/Authentication/authentication.component";
 
 const Shop = () => {
   return <h1>Shop</h1>;
@@ -14,7 +14,7 @@ const App = () => {
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
-        <Route path="sign-in" element={<SignIn />} />
+        <Route path="auth" element={<Authentication />} />
       </Route>
     </Routes>
   );
--- src/Routes/Authentication/authentication.component.jsx
+++ src/Routes/Authentication/authentication.component.jsx
@@ -4,20 +4,18 @@ import {
 } from "../../Utils/Firebase/firebase.utils";
 
 import SignUpForm from "../../Components/Sign-up-form/sign-up-form.component";
+import SignInForm from "../../Components/Sign-in-form/sign-in-form.component";
 
-const SignIn = () => {
-  const logGoogleUser = async () => {
-    const { user } = await signInWithGooglePopup();
-    const userDocRef = await createUserDocumentFromAuth(user);
-  };
+const Authentication = () => {
+
 
   return (
     <div>
       <h1>Sign In page</h1>
-      <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+      <SignInForm />
       <SignUpForm />
     </div>
   );
 };
 
-export default SignIn;
+export default Authentication;
