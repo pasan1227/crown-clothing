e30c9a97080963cf58faf66a9fdeb6ab0ff4a8cb
pasan1227 <pasanratnayake@gmail.com>
2023-10-14 11:54:30 +0530

    Created checkout page with cart functionality

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./Routes/Home/home.component";
 import Navigation from "./Routes/Navigation/navigation.component";
 import Authentication from "./Routes/Authentication/authentication.component";
 import Shop from "./Routes/Shop/shop.component";
+import Checkout from "./Routes/Checkout/checkout.component";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
         <Route path="auth" element={<Authentication />} />
+        <Route path="checkout" element={<Checkout />} />
       </Route>
     </Routes>
   );
