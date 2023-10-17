730d01cd3c2181090744a31e2f3476df2cf2a5b6
pasan1227 <pasanratnayake@gmail.com>
2023-10-17 10:34:02 +0530

    Login bug fixed

--- src/Store/User/user.action.js
+++ src/Store/User/user.action.js
@@ -3,4 +3,4 @@ import { USER_ACTION_TYPES } from "./user.types";
 import { createAction } from "../../Utils/Reducer/reducer.utils";
 
 export const setCurrentUser = (user) =>
-  createAction(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user));
+  createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user);
